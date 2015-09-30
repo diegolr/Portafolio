c87fbbbdda57b23c98edb9f240300c343a322ce3
diegolr <diego.los.re@gmail.com>
2015-09-30 20:37:49 +0200

    Commit 5. Modificada estructura de Nav. Añadida distribución flex en página personal.

--- js/index.js
+++ js/index.js
@@ -15,18 +15,35 @@ buttonContact.on("click", loadContactContent);
 
 function loadMeContent(){
 	$( "#main" ).load( "templates/me.html" );
+	buttonMe.css("background","#FF9C00");
+	buttonStudies.css("background","white");
+	buttonPortfolio.css("background","white");
+	buttonContact.css("background","white");
+	
 }
 
 function loadStudiesContent(){
 	$( "#main" ).load( "templates/studies.html" );
+	buttonMe.css("background","white");
+	buttonStudies.css("background","#FF9C00");
+	buttonPortfolio.css("background","white");
+	buttonContact.css("background","white");
 }
 
 function loadPortfolioContent(){
 	$( "#main" ).load( "templates/portfolio.html" );
+	buttonMe.css("background","white");
+	buttonStudies.css("background","white");
+	buttonPortfolio.css("background","#FF9C00");
+	buttonContact.css("background","white");
 }
 
 function loadContactContent(){
 	$( "#main" ).load( "templates/contact.html" );
+	buttonMe.css("background","white");
+	buttonStudies.css("background","white");
+	buttonPortfolio.css("background","white");
+	buttonContact.css("background","#FF9C00");
 }
 
 })();
