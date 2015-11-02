52954a0f45caa513fdc36cbedcde094bf839a115
diegolr <diego.los.re@gmail.com>
2015-11-02 20:51:13 +0100

    Commit 17. Información en página de contactos añadida. Falta captcha

--- js/studies/experience.js
+++ js/studies/experience.js
@@ -1,9 +1,24 @@
 (function() {
+var buttonIncibe= $("[data-button='incibe']");
 var buttonFaitic = $("[data-button='faitic']");
 var buttonPeritos = $("[data-button='peritos']");
 
+buttonIncibe.on("click", showIncibe);
 buttonFaitic.on("click", showFaitic);
 buttonPeritos.on("click", showPeritos);
+
+function showIncibe(){
+	var className = $("#incibeSpan").attr('class');
+	if(className=='icon-circle-down'){
+		$("#incibeSpan").removeClass('icon-circle-down').addClass('icon-circle-up');
+		$( "#incibe" ).removeClass('StudiesContainer-Item-Content__noActive').addClass('StudiesContainer-Item-Content__active');
+	}
+	if(className=='icon-circle-up'){
+		$("#incibeSpan").removeClass('icon-circle-up').addClass('icon-circle-down');
+		$( "#incibe" ).removeClass('StudiesContainer-Item-Content__active').addClass('StudiesContainer-Item-Content__noActive');
+	}
+	
+}
 
 function showFaitic(){
 	var className = $("#faiticSpan").attr('class');
